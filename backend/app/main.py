2040b6980d60ff7b98cc301a765a9086dea7549b
Vedant Rode <vedant.rode.2005@gmail.com>
2026-09-04 01:35:37 +0530

    done with frotnend and backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,35 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from app.api.routes.dev import router as dev_router
+from app.api.routes.auth import router as auth_router
+from app.core.supabase import supabase
+from app.api.routes.test_auth import router as test_auth_router
+from app.api.routes.reports import router as reports_router
+from app.api.routes.upload import router as upload_router
 
 app = FastAPI(
     title="CAMRA API",
     description="Context-Aware Medical Report Analyzer",
     version="0.1.0"
 )
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+app.include_router(auth_router)
+app.include_router(dev_router)
+app.include_router(test_auth_router)
+app.include_router(reports_router)
+app.include_router(upload_router)
 
 
 @app.get("/")
@@ -20,4 +45,20 @@ def root():
 def health_check():
     return {
         "status": "healthy"
+    }
+
+
+@app.get("/test-db")
+def test_database():
+    response = (
+        supabase
+        .table("reports")
+        .select("id")
+        .limit(1)
+        .execute()
+    )
+
+    return {
+        "database": "connected",
+        "data": response.data
     }
